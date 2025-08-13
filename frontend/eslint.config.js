c6691d1b928cd7e8dfc57c4f1a6c14eadf860d2e
Aaron Zellweger <aaron.zellweger@gemdat.ch>
2025-08-13 08:33:48 +0200

    frontend tools configuration adjustments

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,18 +3,41 @@ const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginBoundaries = require('eslint-plugin-boundaries');
+const eslintImportPlugin = require('eslint-plugin-import');
 
 module.exports = tseslint.config(
   {
     files: ['**/*.ts'],
+    plugins: {
+      'boundaries': eslintPluginBoundaries,
+    },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
       eslintPluginPrettierRecommended,
+      eslintImportPlugin.flatConfigs.recommended,
     ],
     processor: angular.processInlineTemplates,
+    settings: {
+      'import/resolver': {
+        alias: {
+          map: [
+            ['@core', './src/app/core'],
+            ['@shared', './src/app/shared'],
+            ['@features', './src/app/features'],
+          ],
+          extensions: ['.ts', '.js', '.jsx', '.json', '.tsx'],
+        },
+      },
+      'boundaries/elements': [
+        { type: 'core', pattern: 'src/app/core/**' },
+        { type: 'shared', pattern: 'src/app/shared/**' },
+        { type: 'features', pattern: 'src/app/features/**' },
+      ],
+    },
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
@@ -32,6 +55,19 @@ module.exports = tseslint.config(
           style: 'kebab-case',
         },
       ],
+      'boundaries/element-types': [
+        'error',
+        {
+          default: 'disallow',
+          rules: [
+            { from: 'core', allow: ['core', 'shared'] },
+            { from: 'shared', allow: ['shared'] },
+            { from: 'features', allow: ['features', 'shared'] },
+          ],
+        },
+      ],
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
     },
   },
   {
