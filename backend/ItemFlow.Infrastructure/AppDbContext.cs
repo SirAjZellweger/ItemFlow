bcb24588acc3c4148258cac45810f500368682d5
Aaron Zellweger <aaron.zellweger@gemdat.ch>
2025-08-14 10:01:20 +0200

    create backen project, add entity framework with dummy entity

---
+++ backend/ItemFlow.Infrastructure/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ItemFlow.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DummyDbo>(entity =>
+        {
+            entity.ToTable("Dummy");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
